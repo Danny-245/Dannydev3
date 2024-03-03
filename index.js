dd3a097728013ec0da2f4eaee2f8fab8b3fbd3ba
Danny-245 <117903552+Danny-245@users.noreply.github.com>
2024-03-03 18:13:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,28 @@ const nav = document.getElementById('openNav');
 
 openNav.addEventListener('click', function () {
     nav.classList.add('show');
-} );
+});
 hideNav.addEventListener('click', function () {
     nav.classList.remove('show');
 });
 
-var typed = new Typed(".element",{
-    strings: ["Develop websites.","Design Graphics.","Innovate | Educate."],
-    smartBackspace:true,
-    typeSpeed:70,
-    backSpeed:50,
-    loop:true,
-    loopCount:Infinity,
-    startDelay:1000
+var typed = new Typed(".element", {
+    strings: ["Develop websites.", "Design Graphics.", "Innovate | Educate."],
+    smartBackspace: true,
+    typeSpeed: 70,
+    backSpeed: 50,
+    loop: true,
+    loopCount: Infinity,
+    startDelay: 1000
 });
+
+function addEmail() {
+    var emailId = document.getElementById('emailId');
+    var email = document.getElementById('floatingEmail');
+
+    let emailTxt = emailId.value;
+    email.value = emailTxt;
+
+}
+
+
