df2fca3938f442ee736ee97d80f1a48a7ab8113d
Dannydev <developerdannydev@gmail.com>
2026-03-23 14:06:55 +0100

    Fixed login bug and updated UI

--- about.js
+++ about.js
@@ -6,17 +6,7 @@ const nav = document.getElementById('openNav');
 openNav.addEventListener('click', function () {
     nav.classList.add('show');
 } );
-hideNav.addEventListener('click', function () {
+hideNav.addEventListener("click", function(){
     nav.classList.remove('show');
-=======
-const openNav = document.getElementById('open-nav')
-const hideNav = document.getElementById('hideNav');
-const nav = document.getElementById('openNav');
 
-openNav.addEventListener('click', function () {
-    nav.classList.add('show');
-} );
-hideNav.addEventListener('click', function () {
-    nav.classList.remove('show');
->>>>>>> d54b6a89ef99e02ed55ff26cdad5af5d13f1c5bd
 })
